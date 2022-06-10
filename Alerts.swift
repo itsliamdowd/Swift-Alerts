5017b27582a6669f4bfd9609c8380edfeb6a5de6
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-06-10 01:07:35 -0700

    Update Alerts.swift

--- Alerts.swift
+++ Alerts.swift
@@ -3,76 +3,32 @@ import UIKit
 
 class Alerts: UIViewController {
 
-
     @IBOutlet weak var alertView: UIView!
-
-
     @IBOutlet weak var closeButton: UIButton!
-
-
-
-    @IBAction func closeButtonPressed(_ sender: Any) {
-
-
-        self.dismiss(animated: true, completion: nil)
-
-
-    }
-
-
     @IBOutlet weak var alertText: UILabel!
 
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
     var alertDataText: String = "Alert"
 
-
     override func viewDidLoad() {
-
-
         super.viewDidLoad()
-
-
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
-
-
         let blurView = UIVisualEffectView(effect: blurEffect)
-
-
         blurView.frame = view.bounds
-
-
         blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
-
         view.addSubview(blurView)
-
-
         alertView.layer.cornerRadius = 20
-
-
         alertView.layer.masksToBounds = true
-
-
         closeButton.layer.cornerRadius = 10
-
-
         view.sendSubviewToBack(blurView)
-
-
+        
         DispatchQueue.main.async {
-
-
             self.alertText?.text = UserDefaults.standard.string(forKey: "alertText")
-
-
             self.alertText.setContentCompressionResistancePriority(.required, for: .horizontal)
-
-
         }
-
-
     }
-
-
 }
 
